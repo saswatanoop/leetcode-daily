753254d92bb1f6d848628cfe8a3099d76c12ea7a
sswt19 <saswatanoop@gmail.com>
2026-06-03 19:27:00 +0530

    3rd May 2026

---
+++ 2026-06/2026-06-03.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/earliest-finish-time-for-land-and-water-rides-ii/submissions/2021300240/?envType=daily-question&envId=2026-06-03
+
+from typing import List
+
+# T:O(n+m) S:O(1)
+class Solution:
+    def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]):
+        
+        n,m=len(landStartTime), len(waterStartTime)
+        min_water=min_land=float("inf")
+
+        # find min for single use case
+        for i in range(n):
+            min_land=min(min_land , landStartTime[i] + landDuration[i])
+        for i in range(m):
+            min_water=min(min_water, waterStartTime[i]+ waterDuration[i])
+
+        min_time=float("inf")
+
+        
+        # Try 1) land first then water and 
+        for i in range(m):
+            min_time=min(min_time, max(min_land,waterStartTime[i])+waterDuration[i])
+
+        # 2) water first then land, pick min of both
+        for i in range(n):
+            min_time=min(min_time, max(min_water,landStartTime[i])+landDuration[i])
+
+        return min_time
