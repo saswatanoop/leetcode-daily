30067ecb4bbd02da3664ef5d9f760cdd3afdda77
sswt19 <saswatanoop@gmail.com>
2026-07-30 09:33:13 +0530

    LC daily 30th July

---
+++ 2026-07/30th.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-i/
+
+from collections import Counter
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        
+        freq=Counter(word)
+        arr=[v for v in freq.values()]
+        arr.sort(reverse=True)
+
+        count=0
+        for i,v in enumerate(arr):
+            count+=(i//8+1)*v
+
+        
+        return count
